0df57498a094cd4ab6082c3ffda4cb1eef95c077
Vincent.Li <vincentlee99@126.com>
2021-03-09 21:18:32 +0800

    update

--- src/test/java/com/vincent/springboot/webflux/SpringbootWebfluxPractiseApplicationTests.java
+++ src/test/java/com/vincent/springboot/webflux/SpringbootWebfluxPractiseApplicationTests.java
@@ -1,13 +1,11 @@
 package com.vincent.springboot.webflux;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
-public class SpringbootWebfluxPractise01ApplicationTests {
+public class SpringbootWebfluxPractiseApplicationTests {
 
 	@Test
 	public void contextLoads() {
